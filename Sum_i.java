f9fbfa8450d1b8a6b07ab5746fd28fd8a375f80d
Yoshiaki NAGASAWA <ykk.26camellia@gmail.com>
2017-03-23 07:00:51 +0900

    Add Sum_i

---
+++ Sum_i.java
@@ -0,0 +1,27 @@
+import.java.util.Scanner;
+
+class Sum_i{
+
+    public static void main(String[] args){
+    
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("Sum up from 1 to n.")
+            int n;
+            do{
+                System.out.print("n is :");
+                n = stdIn.nextInt();
+            }while(n <= 0);
+
+            int sum = 0;
+            int i = 1;
+
+            while(i <= n);{
+                sum += i;
+                i++;
+            }
+        
+        System.out.println("The sum from 1 to" + n + "is" + sum);
+
+    }
+}
